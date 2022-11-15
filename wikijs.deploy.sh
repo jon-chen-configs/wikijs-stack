49a25a3ddc9efedddde5b57e19e461b4fd353084
Jonathan Chen <jonchen228@gmail.com>
2022-11-14 21:24:35 -0500

    move config to subdirectory.

--- wikijs.deploy.sh
+++ wikijs.deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 WD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-SCRIPTS_DIR=${WD}/../../scripts
-export ENV_DIR=${WD}/../../../compose/conf/swarm-env-config
+SCRIPTS_DIR=${WD}/../../../scripts
+export ENV_DIR=${WD}/../../../../compose/conf/swarm-env-config
 export ENV_FILE=${ENV_DIR}/swarm.env
 export COMPOSE_FILE=${WD}/wikijs-swarm.yml
 export STACK_NAME=wikijs-swarm
--- wikijs.remove.sh
+++ wikijs.remove.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-SCRIPTS_DIR=${WD}/../../scripts
+SCRIPTS_DIR=${WD}/../../../scripts
 export STACK_NAME=wikijs-swarm
 
 ${SCRIPTS_DIR}/stack.remove.sh
